f6063c96611ab5411fff386ef6ae8a932e8e93d1
Om Prabhat <144362728+om-prabhat@users.noreply.github.com>
2025-05-02 01:36:25 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    trailingSlash: true,
+
+    async rewrites() {
+        const development = process.env.NEXT_PUBLIC_ENV === "development";
+        const BASEURL = development
+            ? process.env.NEXT_PUBLIC_DEV_URL
+            : process.env.NEXT_PUBLIC_PROD_URL;
+
+        return [
+            {
+                source: "/api/:path*/",
+                destination: `${BASEURL}/api/:path*/`, // Proxy to the appropriate URL based on the environment
+            },
+        ];
+    },
   images: {
     domains: [
       'encrypted-tbn3.gstatic.com',
