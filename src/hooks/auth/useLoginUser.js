df27871a359ec374df4cd6105e6fcb665a9467d2
p1yux <soulpiyush09@gmail.com>
2025-05-02 02:22:06 +0530

    ...

--- src/hooks/auth/useLoginUser.js
+++ src/hooks/auth/useLoginUser.js
@@ -1,3 +1,5 @@
+"use client"
+
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { loginSchema } from "@/lib/zod/authSchemas";
 import axiosInstance from "@/lib/axios";
--- src/hooks/auth/useLogoutUser.js
+++ src/hooks/auth/useLogoutUser.js
@@ -1,3 +1,5 @@
+"use client"
+
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axiosInstance from "@/lib/axios";
 import { toast } from "sonner";
